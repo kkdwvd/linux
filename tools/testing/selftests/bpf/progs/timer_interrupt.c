fce82a2c4b81b29b1e4938eaeb3cb810138be21f
Kumar Kartikeya Dwivedi <memxor@gmail.com>
2026-07-14 19:48:53 +0200

    selftests/bpf: Test hardirq timer mode
    Extend timer_interrupt to arm one regular BPF timer and one initialized
    with BPF_F_TIMER_HARDIRQ.
    
    Record the callback interrupt state and wait for both timers to fire.
    Verify that the regular callback does not run in hardirq context and that
    the flagged callback does.
    
    Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>

--- tools/testing/selftests/bpf/progs/timer_interrupt.c
+++ tools/testing/selftests/bpf/progs/timer_interrupt.c
@@ -8,9 +8,16 @@ char _license[] SEC("license") = "GPL";
 
 #define CLOCK_MONOTONIC		1
 
-int preempt_count;
 int in_interrupt;
-int in_interrupt_cb;
+int soft_preempt_count;
+int soft_in_interrupt;
+int soft_in_hardirq;
+int hard_preempt_count;
+int hard_in_interrupt;
+int hard_in_hardirq;
+bool soft_fired;
+bool hard_fired;
+int timer_err;
 
 struct elem {
 	struct bpf_timer t;
@@ -18,31 +25,56 @@ struct elem {
 
 struct {
 	__uint(type, BPF_MAP_TYPE_ARRAY);
-	__uint(max_entries, 1);
+	__uint(max_entries, 2);
 	__type(key, int);
 	__type(value, struct elem);
 } array SEC(".maps");
 
-static int timer_in_interrupt(void *map, int *key, struct bpf_timer *timer)
+static int timer_in_interrupt(void *map, int *key, struct elem *value)
 {
-	preempt_count = get_preempt_count();
-	in_interrupt_cb = bpf_in_interrupt();
+	if (*key == 0) {
+		soft_preempt_count = get_preempt_count();
+		soft_in_interrupt = bpf_in_interrupt();
+		soft_in_hardirq = bpf_in_hardirq();
+		soft_fired = true;
+	} else {
+		hard_preempt_count = get_preempt_count();
+		hard_in_interrupt = bpf_in_interrupt();
+		hard_in_hardirq = bpf_in_hardirq();
+		hard_fired = true;
+	}
 	return 0;
+}
+
+static __always_inline int start_timer(int key, __u64 flags)
+{
+	struct elem *value;
+	int err;
+
+	value = bpf_map_lookup_elem(&array, &key);
+	if (!value)
+		return -1;
+
+	err = bpf_timer_init(&value->t, &array, flags);
+	if (err)
+		return err;
+
+	err = bpf_timer_set_callback(&value->t, timer_in_interrupt);
+	if (err)
+		return err;
+
+	return bpf_timer_start(&value->t, 0, BPF_F_TIMER_CPU_PIN);
 }
 
 SEC("fentry/bpf_fentry_test1")
 int BPF_PROG(test_timer_interrupt)
 {
-	struct bpf_timer *timer;
-	int key = 0;
+	in_interrupt = bpf_in_interrupt();
 
-	timer = bpf_map_lookup_elem(&array, &key);
-	if (!timer)
+	timer_err = start_timer(0, CLOCK_MONOTONIC);
+	if (timer_err)
 		return 0;
 
-	in_interrupt = bpf_in_interrupt();
-	bpf_timer_init(timer, &array, CLOCK_MONOTONIC);
-	bpf_timer_set_callback(timer, timer_in_interrupt);
-	bpf_timer_start(timer, 0, 0);
+	timer_err = start_timer(1, CLOCK_MONOTONIC | BPF_F_TIMER_HARDIRQ);
 	return 0;
 }
